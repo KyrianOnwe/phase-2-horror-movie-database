e2a35e2daaa9d368bfa21c4c7a11e1e99e70432c
Kyrian <dr.kyrian.onwe@gmail.com>
2022-05-21 12:22:27 -0500

    Build form for adding movie

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Add = () => {
+    const [movieForm, setMovieForm] = useState({
+        title: "",
+        year: "",
+        subgenre: "",
+        poster: "",
+        director: ""
+    })
+
   return (
-    <div>Add</div>
+    <div>
+        <h1>What should we watch next?</h1>
+        <form>
+            <input type="text" name="title" id="title"/>
+            <input type="text" name="year" id="year"/>
+            <input type="text" name="subgenre" id="subgenre"/>
+            <input type="text" name="poster" id="poster"/>
+            <input type="text" name="director" id="director"/>
+            <button>Submit</button>
+
+        </form>
+    </div>
   )
 }
 
