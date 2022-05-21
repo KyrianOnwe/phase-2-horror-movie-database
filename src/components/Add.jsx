87603a6a46892ab1661f708820f1e4970757c32c
Kyrian <dr.kyrian.onwe@gmail.com>
2022-05-21 12:24:23 -0500

    Add placeholders to form

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -14,11 +14,11 @@ const Add = () => {
     <div>
         <h1>What should we watch next?</h1>
         <form>
-            <input type="text" name="title" id="title"/>
-            <input type="text" name="year" id="year"/>
-            <input type="text" name="subgenre" id="subgenre"/>
-            <input type="text" name="poster" id="poster"/>
-            <input type="text" name="director" id="director"/>
+            <input type="text" name="title" id="title" placeholder='title'/>
+            <input type="text" name="year" id="year" placeholder='year'/>
+            <input type="text" name="subgenre" id="subgenre" placeholder='subgenre'/>
+            <input type="text" name="poster" id="poster" placeholder='poster url'/>
+            <input type="text" name="director" id="director" placeholder='director'/>
             <button>Submit</button>
 
         </form>
