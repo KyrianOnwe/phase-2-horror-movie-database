7e200fe890c77a3ad4425cf40ac8e4dce8109901
Kyrian <dr.kyrian.onwe@gmail.com>
2022-05-21 21:10:21 -0500

    Enable controlled form onChanges

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -10,15 +10,22 @@ const Add = () => {
         director: ""
     })
 
+    function handleSetFormData(event){
+      setMovieForm({
+        ...movieForm,
+        [event.target.name]: event.target.value,
+      });
+    }
+
   return (
     <div>
         <h1>What should we watch next?</h1>
         <form>
-            <input type="text" name="title" id="title" placeholder='title'/>
-            <input type="text" name="year" id="year" placeholder='year'/>
-            <input type="text" name="subgenre" id="subgenre" placeholder='subgenre'/>
-            <input type="text" name="poster" id="poster" placeholder='poster url'/>
-            <input type="text" name="director" id="director" placeholder='director'/>
+            <input type="text" name="title" id="title" placeholder='title' value={movieForm.title} onChange={handleSetFormData} />
+            <input type="text" name="year" id="year" placeholder='year' value={movieForm.year} onChange={handleSetFormData} />
+            <input type="text" name="subgenre" id="subgenre" placeholder='subgenre' value={movieForm.subgenre} onChange={handleSetFormData} />
+            <input type="text" name="poster" id="poster" placeholder='poster url' value={movieForm.poster} onChange={handleSetFormData} />
+            <input type="text" name="director" id="director" placeholder='director' value={movieForm.director} onChange={handleSetFormData} />
             <button>Submit</button>
 
         </form>
@@ -26,4 +33,4 @@ const Add = () => {
   )
 }
 
-export default Add
+export default Add;
