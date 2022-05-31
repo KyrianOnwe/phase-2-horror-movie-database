b175a7a13de30003e85940132af2aef9b8a0fe6d
Kyrian <dr.kyrian.onwe@gmail.com>
2022-05-31 18:40:40 -0500

    Destructure all working props

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import MovieCards from './MovieCards'
 
-const Movies = (props) => {
-  const movies = props.movies;
-  console.log(movies)
+const Movies = ({ movies, isWatched }) => {
+
 
   return (
     <div>
-      {movies.length === 0 ? <h1>Let's get some movies!</h1> : movies.map((movie) => <MovieCards movie={movie.title} id={movie.id} url={movie.poster} subgenre={movie.subgenre} year={movie.year} director={movie.director} isWatched={props.isWatched} key={movie.id} /> )}
+      {movies.length === 0 ? <h1>Let's get some movies!</h1> : movies.map((movie) => <MovieCards movie={movie.title} id={movie.id} url={movie.poster} subgenre={movie.subgenre} year={movie.year} director={movie.director} isWatched={isWatched} key={movie.id} /> )}
     </div>
   )
 }
--- src/components/Watched.jsx
+++ src/components/Watched.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import MovieCards from './MovieCards'
 
-const Watched = (proops) => {
-  const watched = proops.watched
-  // console.log(watched)
+const Watched = ({ message, watched, isWatched }) => {
 
   return (
     <div>
-      {watched.length === 0 ? <h1>{proops.message}</h1> : watched.map((w) => <MovieCards movie={w.title} id={w.id} url={w.poster} subgenre={w.subgenre} year={w.year} director={w.director} key={w.id} isWatched={proops.isWatched}/> )}
+      {watched.length === 0 ? <h1>{message}</h1> : watched.map((w) => <MovieCards movie={w.title} id={w.id} url={w.poster} subgenre={w.subgenre} year={w.year} director={w.director} key={w.id} isWatched={isWatched}/> )}
     </div>
   )
 }
