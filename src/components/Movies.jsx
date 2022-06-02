03d6e1bf076798c00bf3dbe8e542348ff86850cd
Kyrian <dr.kyrian.onwe@gmail.com>
2022-06-01 19:31:17 -0500

    Create className's

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -5,7 +5,7 @@ const Movies = ({ movies, isWatched }) => {
 
 
   return (
-    <div>
+    <div className='movies-container' >
       {movies.length === 0 ? <h1>Let's get some movies!</h1> : movies.map((movie) => <MovieCards movie={movie.title} id={movie.id} url={movie.poster} subgenre={movie.subgenre} year={movie.year} director={movie.director} isWatched={isWatched} key={movie.id} /> )}
     </div>
   )
