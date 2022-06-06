50aa63e7ae5238ee28f520ea4775cf6b9abac649
Kyrian <dr.kyrian.onwe@gmail.com>
2022-06-05 20:37:06 -0500

    Update Movie component text.

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -6,7 +6,7 @@ const Movies = ({ movies, isWatched }) => {
 
   return (
     <div className='movies-container' >
-      {movies.length === 0 ? <h1>Let's get some movies!</h1> : movies.map((movie) => <MovieCards movie={movie.title} id={movie.id} url={movie.poster} subgenre={movie.subgenre} year={movie.year} director={movie.director} isWatched={isWatched} key={movie.id} /> )}
+      {movies.length === 0 ? <h1>Let's get some movies!  Click or tap on "Add a New Flick" to start feeling the fear!</h1> : movies.map((movie) => <MovieCards movie={movie.title} id={movie.id} url={movie.poster} subgenre={movie.subgenre} year={movie.year} director={movie.director} isWatched={isWatched} key={movie.id} /> )}
     </div>
   )
 }
