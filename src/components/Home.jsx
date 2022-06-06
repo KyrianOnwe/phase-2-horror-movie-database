49d58477ec6932d3e4f5aa4c81d6843f1a9f6a66
Kyrian <dr.kyrian.onwe@gmail.com>
2022-06-05 20:31:48 -0500

    Update Home component text.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,9 @@ const Home = () => {
     <div id='home-screen'>
       <p>A horror movie is an experience of abject terror without the benefit of any lightness whatsoever.  A good horror movie does not 'resolve' its dramatic elements, as such, but instead decreases the tension from a solid 10 to 'just enough to let you sleep'...for now.  But the fear will never abate.  This app is ONLY for GOOD horror movies.  Add them, watch them and never forget how they made you feel and we won't forget what it was that made you feel it!</p>
       <br />
-      <p>To use this app, start by adding a title, the year of release, a brief description or subgenre, the url for the movie, and the director's name and keep track of the hottest and sickest new flicks!</p>
+      <p>To use this app, after initial setup, start by clicking and going to the "Add" tab and add a title, the year of release, a brief description or subgenre, the url for the movie, and the director's name and keep track of the hottest and sickest new flicks!</p>
       <br />
-      <p>After experiencing the torment, click or tap on the picture to add that title to your 'do not do THIS in the dark, alone, again' list--aka, the Watched list!</p>
+      <p>After experiencing the torment, click or tap on the picture in your "Movies" tab, to add that title to your 'do not do THIS in the dark, alone, again' list--aka, the "Watched" tab!</p>
       <br />
       <p>So come on, hang the light at the door, gather the snacks, and abandon all hope ye that enter here, again, and again, and again...</p>
     </div>
