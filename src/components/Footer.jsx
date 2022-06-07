442b688d040174acedcd02765e5143fafad7556b
Kyrian <dr.kyrian.onwe@gmail.com>
2022-06-07 12:29:20 -0500

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
-        <p>Copyright &copy; 2022, Flatiron Horror App </p>
+        <p>Copyright &copy; 2022, Flatiron Horror App, by Kyrian Onwe</p>
     </footer>
   )
 }
