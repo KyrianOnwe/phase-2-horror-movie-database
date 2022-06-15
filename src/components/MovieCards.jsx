19f7e04e1377c428431df86bef8341a55e261ef7
Kyrian <dr.kyrian.onwe@gmail.com>
2022-06-14 23:12:45 -0500

    Add alt to img's

--- src/components/MovieCards.jsx
+++ src/components/MovieCards.jsx
@@ -5,7 +5,7 @@ const MovieCards = ({ movie, id, url, subgenre, year, director, isWatched }) => 
 
   return (
     <div className={"movie-card"} id={id} onClick={() => isWatched(id)}>
-      <img src={url} />
+      <img src={url} alt={movie} />
       <h3>{movie}</h3>
       <p>A {subgenre} horror film from {year}.  Directed by {director}. </p>
     </div>
